dcc5d0b532496484151fffc07f98bc28f06be764
Fletcher Nichol <fnichol@nichol.ca>
2012-10-01 14:35:51 -0600

    Initial cookbook skeleton.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Fletcher Nichol"
+maintainer_email "fnichol@bluebox.net"
+license          "Apache 2.0"
+description      "Project Razor"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
+supports "ubuntu"
