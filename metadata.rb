cf9aca3ae6955e88be242855a953251d29484522
Fletcher Nichol <fnichol@nichol.ca>
2012-10-02 14:09:28 -0600

    Update metadata.rb.

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,12 @@
-maintainer       "Fletcher Nichol"
-maintainer_email "fnichol@bluebox.net"
-license          "Apache 2.0"
-description      "Project Razor"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+name              "razor"
+maintainer        "Fletcher Nichol"
+maintainer_email  "fnichol@bluebox.net"
+license           "Apache 2.0"
+description       "Project Razor"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "0.1.0"
 
-supports "ubuntu", ">= 12.04"
+supports "ubuntu"
 
 depends "build-essential"
 depends "git"
