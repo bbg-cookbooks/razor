c0acebf1038db5da14dd9a766be3eaa95b5fe6c5
Fletcher Nichol <fnichol@nichol.ca>
2013-06-13 14:38:35 -0600

    Release 0.5.0.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "fnichol@nichol.ca"
 license           "Apache 2.0"
 description       "Project Razor"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.4.1"
+version           "0.5.0"
 
 supports "ubuntu"
 
